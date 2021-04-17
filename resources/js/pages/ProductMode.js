655071bcaf4ef0be9fc0fc7d88e0ed7a95466c95
herilesmana <68384962+herilesmana@users.noreply.github.com>
2021-04-17 10:08:28 +0700

    Working on search functionality

--- resources/js/pages/ProductMode.js
+++ resources/js/pages/ProductMode.js
@@ -4,6 +4,7 @@ import {priceFormat} from '../utils/helper'
 
 const ProductMode = (props) => {
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState(null)
 
     const { addToCart } = props
 
@@ -25,12 +26,14 @@ const ProductMode = (props) => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                     </div>
-                    <input type="text" className="bg-white rounded-3xl shadow text-lg full w-full h-16 py-4 pl-16 transition-shadow focus:shadow-2xl focus:outline-none" placeholder="Cari menu ..." />
+                    <input onChange={(e) => setSearch(e.target.value)} type="text" className="bg-white rounded-3xl shadow text-lg full w-full h-16 py-4 pl-16 transition-shadow focus:shadow-2xl focus:outline-none" placeholder="Cari menu ..." />
                     </div>
                     <div className="h-full overflow-hidden mt-4">
                     <div className="h-full overflow-y-auto px-2">
+                        
                         {/* Ini untuk kalo ga ada product sama sekali */}
-                        {/* <div className="select-none bg-blue-gray-100 rounded-3xl flex flex-wrap content-center justify-center h-full opacity-25">
+                        { products.length === 0 &&
+                        <div className="select-none bg-blue-gray-100 rounded-3xl flex flex-wrap content-center justify-center h-full opacity-25">
                             <div className="w-full text-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
@@ -41,24 +44,40 @@ const ProductMode = (props) => {
                                 ANY PRODUCTS TO SHOW
                                 </p>
                             </div>
-                        </div> */}
+                        </div>
+                        }
+
                         {/* Ini untuk jika hasil pencarian kosong */}
-                        {/* <div className="select-none bg-blue-gray-100 rounded-3xl flex flex-wrap content-center justify-center h-full opacity-25">
-                            <div className="w-full text-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                </svg>
-                                <p className="text-xl">
-                                EMPTY SEARCH RESULT
-                                <br/>
-                                "<span className="font-semibold"></span>"
-                                </p>
+                        { ( products.filter((data)=>{
+                                if(search == null)
+                                    return data
+                                else if(data.name.toLowerCase().includes(search.toLowerCase())){
+                                    return data
+                                }
+                            }).length === 0 && search != null) &&
+                            <div className="select-none bg-blue-gray-100 rounded-3xl flex flex-wrap content-center justify-center h-full opacity-25">
+                                <div className="w-full text-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                    </svg>
+                                    <p className="text-xl">
+                                    EMPTY SEARCH RESULT
+                                    <br/>
+                                    "<span className="font-semibold"></span>"
+                                    </p>
+                                </div>
                             </div>
-                        </div> */}
+                        }
 
                         {/* Ini untuk list product */}
                         <div className="grid grid-cols-4 gap-4 pb-3">
-                            { products.map(item => (
+                            { products.filter((data)=>{
+                                    if(search == null)
+                                        return data
+                                    else if(data.name.toLowerCase().includes(search.toLowerCase())){
+                                        return data
+                                    }
+                                }).map(item => (
                                 <div onClick={() => addToCart(item)} key={item.id} role="button" className="select-none cursor-pointer transition-shadow overflow-hidden rounded-2xl bg-white shadow hover:shadow-lg" >
                                     <img src={item.image} alt={item.name} />
                                     <div className="flex pb-3 px-3 text-sm -mt-3">
